1136bed5dcb8a9c449e99a8402d9c84a51d52d57
Facundo <lujan.facundo@hotmail.com>
2016-02-12 04:21:20 +0100

    Import of tsv files. ToDo:Change absolute path to relative path

---
+++ sql/Import.sql
@@ -0,0 +1,3 @@
+LOAD DATA LOCAL INFILE 'C:/Users/Facu/Desktop/Modificar/new/country.tsv' INTO TABLE country FIELDS TERMINATED BY '\t';
+LOAD DATA LOCAL INFILE 'C:/Users/Facu/Desktop/Modificar/new/indicator.tsv' INTO TABLE indicator FIELDS TERMINATED BY '\t';
+LOAD DATA LOCAL INFILE 'C:/Users/Facu/Desktop/Modificar/new/value.tsv' INTO TABLE value FIELDS TERMINATED BY '\t';
